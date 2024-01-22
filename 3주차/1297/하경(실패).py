225197447551154f1b9bb9e5fd85827e61721892
Mia-HaGyeong <129933771+Mia-HaGyeong@users.noreply.github.com>
2024-01-22 17:10:58 +0900

    Create 하경(실패).py

---
+++ 3주차/1297/하경(실패).py
@@ -0,0 +1,11 @@
+# 대각선 길이와 높이, 너비 비율 입력받기
+D, H, W = map(int, input().split())
+
+# 피타고라스의 정의와 비율을 이용해 실제 높이와 너비 구하기
+
+# 실수형 결과를 정수형으로 변환하여 출력
+print(int(x), int(y))
+
+# d,h,w = map(int,input().split())
+# r = d/(h**2+w**2)**0.5
+# print(int(h*r),int(w*r))
