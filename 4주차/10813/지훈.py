94eee0e5814a47bba570d213f9e8b01b34ad2fd7
jihun <79701544+yangjihun@users.noreply.github.com>
2024-01-29 18:08:09 +0900

    Create 지훈.py

---
+++ 4주차/10813/지훈.py
@@ -0,0 +1,12 @@
+# input
+N,M = map(int,input().split())
+basket = [x for x in range(1,N+1)]
+
+# swap
+for _ in range(M):
+  a,b = map(int,input().split())
+  basket[a-1],basket[b-1] = basket[b-1],basket[a-1]
+
+# print
+for i in basket:
+  print(i, end=' ')
